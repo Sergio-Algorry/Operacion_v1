10bbcd08eea74f76d553d56cb2cbe671c33b95ec
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-03-16 20:02:50 -0300

    Fin de version 1

--- Operacion_v1/Form1.Designer.cs
+++ Operacion_v1/Form1.Designer.cs
@@ -28,13 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtOper = new System.Windows.Forms.TextBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.btRes = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA.Location = new System.Drawing.Point(32, 27);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 38);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB.Location = new System.Drawing.Point(184, 27);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 38);
+            this.txtB.TabIndex = 1;
+            // 
+            // txtOper
+            // 
+            this.txtOper.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOper.Location = new System.Drawing.Point(144, 28);
+            this.txtOper.Name = "txtOper";
+            this.txtOper.Size = new System.Drawing.Size(27, 38);
+            this.txtOper.TabIndex = 2;
+            this.txtOper.Text = "+";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(376, 31);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 31);
+            this.lblRes.TabIndex = 3;
+            // 
+            // btRes
+            // 
+            this.btRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRes.Location = new System.Drawing.Point(290, 25);
+            this.btRes.Name = "btRes";
+            this.btRes.Size = new System.Drawing.Size(75, 41);
+            this.btRes.TabIndex = 4;
+            this.btRes.Text = "=";
+            this.btRes.UseVisualStyleBackColor = true;
+            this.btRes.Click += new System.EventHandler(this.btRes_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(290, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(376, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(144, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 38);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "+";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(184, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 38);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(32, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 38);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(483, 152);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btRes);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.txtOper);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtOper;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Button btRes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Operacion_v1/Form1.cs
+++ Operacion_v1/Form1.cs
@@ -16,5 +16,79 @@ namespace Operacion_v1
         {
             InitializeComponent();
         }
+
+        private void btRes_Click(object sender, EventArgs e)
+        {
+            int A = System.Convert.ToInt32(txtA.Text);
+            int B = System.Convert.ToInt32(txtB.Text);
+            decimal Res = 0;
+
+            if (txtOper.Text == "+")
+            {
+                Res = A + B;
+            }
+            else if (txtOper.Text == "-") 
+            {
+                Res = A - B;
+            }
+            else if (txtOper.Text == "*")
+            {
+                Res = A * B;
+            }
+            else if (txtOper.Text == "/")
+            {
+                Res = A / B;
+            }
+            else
+            {
+                Res = -999999999;
+            }
+
+            if (Res == -999999999)
+            {
+                lblRes.Text = "ERROR";
+            }
+            else
+            {
+                lblRes.Text = Res.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int A = System.Convert.ToInt32(txtA.Text);
+            int B = System.Convert.ToInt32(txtB.Text);
+            decimal Res = 0;
+
+            if (txtOper.Text == "+")
+            {
+                Res = A + B;
+            }
+            else if (txtOper.Text == "-")
+            {
+                Res = A - B;
+            }
+            else if (txtOper.Text == "*")
+            {
+                Res = A * B;
+            }
+            else if (txtOper.Text == "/")
+            {
+                Res = A / B;
+            }
+            else
+            {
+                Res = -999999999;
+            }
+
+            if (Res == -999999999)
+            {
+                lblRes.Text = "ERROR";
+            }
+            else
+            {
+                lblRes.Text = Res.ToString();
+            }
+        }
     }
 }
